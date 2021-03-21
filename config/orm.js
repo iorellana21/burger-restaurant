8213d4628bd717284fd1faa951bf9cdc708fd455
Ivan Orellana Jr <iorellana21@gmail.com>
2021-03-20 22:50:17 -0400

    orm.js DOUBLE CHECK

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,5 @@
 // call database from mysql-connection
-var database = require('../config/mysql-connection');
+var database = require('./mysql-connection');
 
 // create MySQL query methods to be used to retrieve and store data in burgers_db
 // selectAll()
@@ -17,33 +17,33 @@ var orm = {
 
             cb(result);
         });
+    },
+    create: function (table, values, cb) {
+        let query = "INSERT INTO ?? SET ?";
+
+        console.log(query);
+
+        database.query(query, [table, values], (err, result) => {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+    update: function (table, values, condition, cb) {
+        let query = "UPDATE ?? SET ? WHERE ?";
+
+        console.log(query);
+
+        database.query(query, [table, values, condition], (err, result) => {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
     }
-    // create: function (table, values, cb) {
-    //     let query = "INSERT INTO ?? SET ?";
-
-    //     console.log(query);
-
-    //     database.query(query, [table, values], (err, result) => {
-    //         if (err) {
-    //             throw err;
-    //         }
-
-    //         cb(result);
-    //     });
-    // },
-    // update: function (table, values, condition, cb) {
-    //     let query = "UPDATE ?? SET ? WHERE ?";
-
-    //     console.log(query);
-
-    //     database.query(query, [table, values, condition], (err, result) => {
-    //         if (err) {
-    //             throw err;
-    //         }
-
-    //         cb(result);
-    //     });
-    // }
 };
 
 module.exports = orm;

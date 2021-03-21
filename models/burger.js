968cfbfdf69db24d50993ac94ded4f8df5d28f54
Ivan Orellana Jr <iorellana21@gmail.com>
2021-03-20 22:53:31 -0400

    burger.js GOOD TO GO

--- models/burger.js
+++ models/burger.js
@@ -7,17 +7,17 @@ var burger = {
         orm.all("burgers", function (res) {
             cb(res);
         });
+    },
+    create: function (cols, vals, cb) {
+        orm.create("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+    update: function (objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
     }
-    // create: function (values, cb) {
-    //     orm.create("burgers", values, function (res) {
-    //         cb(res);
-    //     });
-    // },
-    // update: function (objColVals, condition, cb) {
-    //     orm.update("burgers", objColVals, condition, function (res) {
-    //         cb(res);
-    //     });
-    // }
 };
 
 module.exports = burger;
